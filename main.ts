2b863c6726c37761fc5a7ec6a21a1d97ce6021b3
Yoshika Takada <touch@rnsk.net>
2019-09-24 16:02:36 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% weight=70 icon="\uf544" color=#EC7505
 namespace irRobot {
     //% blockId=show_strings block="コメント %v"
-    export function noaction(text: string): void {
+    export function nonaction(text: string): void {
     }
 }
