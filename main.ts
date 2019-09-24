4f7b1febd682a25223028529023c645f87509c09
Yoshika Takada <touch@rnsk.net>
2019-09-24 15:58:48 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\uf544" color=#EC7505
+namespace irRobot {
+    //% blockId=show_strings block="コメント %v"
+    export function noaction(text: string): void {
+    }
+}
